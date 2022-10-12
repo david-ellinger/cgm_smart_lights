6eca01deb0b4041bb4fcf5ffb8ed7b53aa40714e
David Ellinger <david.ellinger@me.com>
2022-10-11 22:53:55 -0400

    Added daemon thread to continuously poll

--- app.py
+++ app.py
@@ -16,11 +16,19 @@ hue_bridge_username = os.getenv("HUE_BRIDGE_USERNAME")
 app = Flask(__name__, template_folder="templates")
 converter = Converter()
 
+SECONDS_TO_SLEEP = 60
+
+def update_lights_workflow():
+    bg = dexcom.get_current_glucose_reading()
+    x,y = calculate_color(bg.value)
+    light_change_result = change_color(x,y)
+    print(light_change_result)
+
 def interval_query():
     while True:
-        print("Sleeping for 30 seconds...")
-        time.sleep(30)
         update_lights_workflow()
+        print(f"Sleeping for {SECONDS_TO_SLEEP} seconds...")
+        time.sleep(SECONDS_TO_SLEEP)
 
 thread = threading.Thread(name='interval_query', target=interval_query)
 thread.setDaemon(True)
@@ -41,29 +49,41 @@ def home():
         "home.html",
         title="Glucose Reading <> Light Transition",
         reading=f"Glucose Reading: {bg.value} {bg.trend_arrow} {bg.time}",
+        light_color=f"({x},{y})",
         light_change=str(light_change_result)
     )
 
-def update_lights_workflow():
-    bg = dexcom.get_current_glucose_reading()
-    print(bg)
-    x,y = calculate_color(bg.value)
-    light_change_result = change_color(x,y)
-    print(light_change_result)
+
 
 def calculate_color(glucose_value):
-    if glucose_value <= 75:
+    if glucose_value <= 60:
+        print(f"Glucose Value is {glucose_value} and color is Red")
         return converter.rgb_to_xy(255,0,0) # Red
-    elif glucose_value > 75 and glucose_value <= 95:
-        return converter.rgb_to_xy(255, 255, 0) # Yellow
-    elif glucose_value > 95 and glucose_value <= 130:
-        return converter.rgb_to_xy(0, 255, 0) # Red
-    elif glucose_value > 130 and glucose_value <= 175:
-        return converter.rgb_to_xy(255, 255, 0) # Yellow
-    elif glucose_value > 130 and glucose_value <= 175:
-        return converter.rgb_to_xy(0, 255, 0) # Red
+    elif glucose_value > 60 and glucose_value <= 70:
+        print(f"Glucose Value is {glucose_value} and color is Red->Yellow ")
+        return converter.rgb_to_xy(246, 109, 0) # Red -> Yellow
+    elif glucose_value > 70 and glucose_value <= 80:
+        print(f"Glucose Value is {glucose_value} and color is Yellow ")
+        return converter.rgb_to_xy(215, 167, 0) # Yellow
+    elif glucose_value > 80 and glucose_value <= 90:
+        print(f"Glucose Value is {glucose_value} and color is Yellow -> Green")
+        return converter.rgb_to_xy(160, 214, 0) # Yellow -> Green
+    elif glucose_value > 100 and glucose_value <= 130:
+        print(f"Glucose Value is {glucose_value} and color is Green")
+        return converter.rgb_to_xy(0, 255, 0) # Green
+    elif glucose_value > 130 and glucose_value <= 140:
+        print(f"Glucose Value is {glucose_value} and color is Green -> Yellow")
+        return converter.rgb_to_xy(160, 214, 0) # Green -> Yellow
+    elif glucose_value > 140 and glucose_value <= 150:
+        print(f"Glucose Value is {glucose_value} and color is Yellow")
+        return converter.rgb_to_xy(215, 167, 0) # Yellow
+    elif glucose_value > 150 and glucose_value <= 160:
+        print(f"Glucose Value is {glucose_value} and color is Yellow -> Red")
+        return converter.rgb_to_xy(246, 109, 0) # Yellow -> Red
     else:
-        return converter.rgb_to_xy(0, 0, 255) # Blue
+        print(f"Glucose Value is {glucose_value} and color is Red")
+        return converter.rgb_to_xy(255,0,0) # Red
+
 
 def change_color(x, y):
     headers = {
@@ -76,24 +96,7 @@ def change_color(x, y):
         headers=headers,
         data=data,
     )
-    print(result.json())
     return result.json()
-
-
-def rgb2xyb(r, g, b):
-    r = ((r + 0.055) / 1.055) ** 2.4 if r > 0.04045 else r / 12.92
-    g = ((g + 0.055) / 1.055) ** 2.4 if g > 0.04045 else g / 12.92
-    b = ((b + 0.055) / 1.055) ** 2.4 if b > 0.04045 else b / 12.92
-
-    X = r * 0.4124 + g * 0.3576 + b * 0.1805
-    Y = r * 0.2126 + g * 0.7152 + b * 0.0722
-    Z = r * 0.0193 + g * 0.1192 + b * 0.9505
-
-    return X / (X + Y + Z), Y / (X + Y + Z), int(Y * 254)
-
-
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
