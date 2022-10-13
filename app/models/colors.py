a3c62f95dadc9c90f6b82c2ea8b81016bb701f26
David Ellinger <david.ellinger@me.com>
2022-10-13 00:33:58 -0400

    Moved colors to model

---
+++ app/models/colors.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Colors(Enum):
+    BLUE = (0, 0, 255)
+    RED = (255, 0, 0)
+    RED_YELLOW = (255, 193, 0)
+    YELLOW = (255, 255, 0)
+    YELLOW_GREEN = (214, 255, 0)
+    GREEN = (0, 255, 0)
+    PURPLE = (103, 78, 167)
