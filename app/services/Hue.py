7f7e2fa3e49007dd9e6888e1a2db33e5f904001f
David Ellinger <david.ellinger@me.com>
2022-11-30 10:44:10 -0500

    Brightness set to 50 by default

--- app/services/Hue.py
+++ app/services/Hue.py
@@ -3,14 +3,14 @@ import os
 
 hue_bridge_ip = os.getenv("HUE_BRIDGE_IP")
 hue_bridge_username = os.getenv("HUE_BRIDGE_USERNAME")
-
+DEFAULT_BRIGHTNESS = 50
 class Hue():
     def change_color(self, x, y):
         headers = {
             "Accept": "application/json",
         }
         light = 3
-        data = '{"on":true,"xy":[%.4f,%.4f]}' % (x, y)
+        data = '{"on":true,"bri": %d, "xy":[%.4f,%.4f]}' % (DEFAULT_BRIGHTNESS, x, y)
         result = requests.put(
             f"http://{hue_bridge_ip}/api/{hue_bridge_username}/lights/{light}/state",
             headers=headers,
